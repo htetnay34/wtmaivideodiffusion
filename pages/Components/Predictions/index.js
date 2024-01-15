3b3c44c19ed073bc0140819d6434cecd6b85b015
htetnay34 <132404834+htetnay34@users.noreply.github.com>
2024-01-16 01:20:35 +0630

    Update index.js

--- pages/Components/Predictions/index.js
+++ pages/Components/Predictions/index.js
@@ -81,17 +81,7 @@ function translateToEnglish(text) {
     }
   };
 
-const handleInputChange = (e) => {
-  const inputText = e.target.value;
-  // Update state or perform any logic based on the input text
-};
 
-const shouldShowGoButton = () => {
-  // Add logic here to determine whether to show the "Go!" button
-  // For example, you can use the hasMyanmarCharacters function
-  const inputText = document.getElementsByName("prompt")[0].value;
-  return !hasMyanmarCharacters(inputText);
-};
 
 
   
